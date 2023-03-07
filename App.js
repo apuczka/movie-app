34ff1bfd780580b865b67536f0282734c46469b4
apuczka <a.puczka@icloud.com>
2023-03-07 13:46:07 +0100

    add component AddMovie

--- App.js
+++ App.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect,useCallback } from "react";
 
-import MoviesList from './components/MoviesList';
-import AddMovie from './components/AddMovie';
-import './App.css';
+import MoviesList from "./components/MoviesList";
+import "./App.css";
+import AddMovie from "./components/AddMovie";
 
 function App() {
   const [movies, setMovies] = useState([]);
@@ -13,11 +13,10 @@ function App() {
     setIsLoading(true);
     setError(null);
     try {
-      const response = await fetch('https://swapi.dev/api/films/');
+      const response = await fetch("https://swapi.dev/api/films");
       if (!response.ok) {
-        throw new Error('Something went wrong!');
+        throw new Error("Something went wrong!");
       }
-
       const data = await response.json();
 
       const transformedMovies = data.results.map((movieData) => {
@@ -29,19 +28,19 @@ function App() {
         };
       });
       setMovies(transformedMovies);
+      setIsLoading(false);
     } catch (error) {
       setError(error.message);
     }
     setIsLoading(false);
-  }, []);
 
-  useEffect(() => {
-    fetchMoviesHandler();
-  }, [fetchMoviesHandler]);
+  }, [])
 
-  function addMovieHandler(movie) {
-    console.log(movie);
-  }
+  
+  useEffect(()=> {
+    fetchMoviesHandler()
+  }, [fetchMoviesHandler])
+
 
   let content = <p>Found no movies.</p>;
 
@@ -50,22 +49,23 @@ function App() {
   }
 
   if (error) {
-    content = <p>{error}</p>;
+    content = <p>{error}</p>
+
   }
 
   if (isLoading) {
-    content = <p>Loading...</p>;
+    content = <p>Loading...</p>
   }
 
   return (
     <React.Fragment>
-      <section>
-        <AddMovie onAddMovie={addMovieHandler} />
-      </section>
+    <sectio>
+    <AddMovie onAddMovi></AddMovie></sectio>
       <section>
         <button onClick={fetchMoviesHandler}>Fetch Movies</button>
       </section>
       <section>{content}</section>
+    
     </React.Fragment>
   );
 }
